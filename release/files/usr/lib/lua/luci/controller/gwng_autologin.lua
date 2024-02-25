54530f6d7a9a05d39447689f0e7fb355c99e78c7
barryblueice <barryblueice@outlook.com>
2024-02-25 23:22:14 +0800

    Bug Fixed

--- release/files/usr/lib/lua/luci/controller/gwng_autologin.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.gwng_autologin", package.seeall)
-
-function index()
-    entry({"admin", "services", "gwng_autologin"}, cbi("gwng_autologin"), _("GWNG AutoLogin Client"), 1)
-end
